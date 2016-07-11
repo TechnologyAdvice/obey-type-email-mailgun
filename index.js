b57dca125c7456a1607f4594f13a74ce05843b40
fluidbyte <kent@fluidbyte.net>
2016-07-11 16:05:24 -0400

    Use rp opts instead of one long query

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var Promise = require('bluebird')
 
 module.exports = function(obey, mailgunPublicKey) {
   obey.type('mailgunEmail', function(context) {
-    return rp('https://api:' + mailgunPublicKey + '@api.mailgun.net/v3/address/validate?address=' + encodeURIComponent(context.value))
-      .then(function(results) {
-        if (!JSON.parse(results).is_valid) {
-          context.fail(context.value + ' is an invalid email')
-        }
-      })
+    return rp({
+        uri: 'https://api:' + mailgunPublicKey + '@api.mailgun.net/v3/address/validate',
+        qs: {
+          address: context.value
+        },
+        json: true
+    })
+    .then(function(results) {
+      if (!results.is_valid) {
+        context.fail(context.value + ' is an invalid email')
+      }
+    })
   })
 }
